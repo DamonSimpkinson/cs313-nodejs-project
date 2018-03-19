b78a3a86a4650c101d73891a2eae860c41a912e3
Damon Simpkinson <damonsimpkinson@gmail.com>
2018-03-18 20:53:41 -0400

    add index.js file

---
+++ index.js
@@ -0,0 +1,30 @@
+var bodyParser = require('body-parser');
+var express = require('express');
+var fs = require('fs');
+var path = require('path');
+var themeparks = require('themeparks');
+var url = require('url');
+
+var app = express();
+
+var port = (process.env.PORT || 5000);
+
+app.use(express.static(__dirname + 'public'));
+app.set('views', path.join(__dirname, '/views'));
+app.set('view engine', 'ejs');
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, function(){
+  console.log('Running on port', port);
+});
