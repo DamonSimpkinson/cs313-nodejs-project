f12a8d3935b90d234b1234b3e23e549900545ffb
Damon Simpkinson <damonsimpkinson@gmail.com>
2018-04-07 18:44:41 -0400

    removed some unused lines from file

--- index.js
+++ index.js
@@ -26,26 +26,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // set the port
 app.set('port', (process.env.PORT || 5000));
 
-/*
-// Read the DATABASE_URL environment variable and then parse it out. into the various parts.
-var db_url = url.parse(process.env.DATABASE_URL);
-
-var scheme = db_url.protocol.substr(0, db_url.protocol.length - 1);
-var user = db_url.auth.substr(0, db_url.auth.indexOf(':'));
-var pass = db_url.auth.substr(db_url.auth.indexOf(':') + 1, db_url.auth.length);
-var host = db_url.host.substr(0, db_url.host.indexOf(':'));
-var pgport = db_url.host.substr(db_url.host.indexOf(':') + 1, db_url.host.length);
-var db = db_url.path.substr(db_url.path.indexOf('/') + 1, db_url.path.length);
-
-// Connect to the PostgreSQL server
-const client = new Client({
-    host: host,
-    user: user,
-    database: db,
-    password: pass
-});
-*/
-
 const client = new Client({
   connectionString: process.env.DATABASE_URL,
   ssl: true,
